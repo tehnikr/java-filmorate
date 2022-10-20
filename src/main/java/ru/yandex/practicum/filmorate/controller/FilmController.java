1fc8e56e2a5cae00678ce7d8711c24347ba6358f
Tehnikr <bswcct@gmail.com>
2022-10-20 11:56:34 +0300

    3

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -3,36 +3,56 @@ package ru.yandex.practicum.filmorate.controller;
 import ch.qos.logback.classic.Level;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping(
+        value = "/films"
+)
 public class FilmController {
     private static final Logger log = LoggerFactory.getLogger(FilmController.class);
     private final List<Film> films = new ArrayList<>();
 
-    @GetMapping("/films")
+    @GetMapping
     public List<Film> findAll() {
         ch.qos.logback.classic.Logger l = (ch.qos.logback.classic.Logger) log;
         l.setLevel(Level.TRACE);
         log.trace("Текущее количество фильмов: " + films.size());
         return films;
     }
-    @PostMapping("/films")
-    public List<Film> create(@RequestBody Film film) {
+
+    @PostMapping
+    public Film saveFilm(@RequestBody @Valid Film film) {
 
         ch.qos.logback.classic.Logger l = (ch.qos.logback.classic.Logger) log;
         l.setLevel(Level.TRACE);
-        log.trace("Добавление фильма log: " + films.size());
-        System.out.println("Добавление фильма");
-        film.setId(films.size()+1);
+
+
+        film.setId(films.size() + 1);
+
         films.add(film);
+        log.trace("Добавление фильма log: " + films.size());
+        return film;
+    }
+
+    @PutMapping("/films")
+    public List<Film> put(@RequestBody Film film) {
+        ch.qos.logback.classic.Logger l = (ch.qos.logback.classic.Logger) log;
+        l.setLevel(Level.TRACE);
+        log.trace("Изменение фильма: " + films.size());
+
+        System.out.println("Новые данные: " + film.toString());
+
         return films;
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -4,13 +4,22 @@ import lombok.Data;
 
 import java.time.Duration;
 import java.time.Instant;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 public class Film {
     int id;
+
+    @NotBlank(message = "name should not be blank")
     String name;
+    @Size(max=200, message = "description should not be greater 200")
     String description;
     String releaseDate;
+
+    @Min(value = 0, message = "duration should be greater than 0")
     int duration;
 
 }
